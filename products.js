790b82f3b7eba2d66f27bbd8e72ea6dc896ece88
harshasaiakhil <harshasaiakhilb@gmail.com>
2025-01-20 14:16:11 +0530

    commit files

--- products.js
+++ products.js
@@ -34,62 +34,62 @@ function createElement(src, name, abtPro) {
 
 let productslist = [
   {
-    src: "/img/solar_panel_img.jpg",
+    src: "./img/solar_panel_img.jpg",
     name: "Solar Panels",
     abtPro:"Efficient, durable solar panels to harness clean energy and reduce your electricity bills."
   },
   {
-    src: "/img/solar_water_heaters_img.jpg",
+    src: "./img/solar_water_heaters_img.jpg",
     name: "solar water heaters",
     abtPro:"Eco-friendly water heating systems powered by the sun, perfect for sustainable living."
   },
   {
-    src:"/img/solar_generator_img.jpg",
+    src:"./img/solar_generator_img.jpg",
     name:"solar generators",
     abtPro:"Portable and reliable solar-powered energy backup for your home or outdoor adventures."
   },
   {
-    src:"/img/solar_home_battery_img.jpg",
+    src:"./img/solar_home_battery_img.jpg",
     name:"home battery systems",
     abtPro:"Store energy for use anytime with advanced, long-lasting home battery systems."
   },
   {
-    src:"/img/portable_battery_img.jpg",
+    src:"./img/portable_battery_img.jpg",
     name:"portable battery packs",
     abtPro:"Compact and lightweight power solutions for charging devices on the go."
   },
   {
-    src:"/img/biogas_digester_img.jpeg",
+    src:"./img/biogas_digester_img.jpeg",
     name:"biogas digesters",
     abtPro:"Convert organic waste into renewable energy with these efficient biogas systems."
   },
   {
-    src:"/img/biomass_boilers_img.jpg",
+    src:"./img/biomass_boilers_img.jpg",
     name:"biomass boilers",
     abtPro:"Sustainable heating solutions using natural biomass fuels for eco-conscious homes."
   },
   {
-    src:"/img/smart_meters_img.jpg",
+    src:"./img/smart_meters_img.jpg",
     name:"smart meters",
     abtPro:"Track and optimize energy usage with real-time data from advanced smart meters."
   },
   {
-    src:"/img/off_grid_kits_img.jpg",
+    src:"./img/off_grid_kits_img.jpg",
     name:"off grid kits",
     abtPro:"Comprehensive off-grid solutions for remote locations or self-sufficient living."
   },
   {
-    src:"/img/solar_powered_ev_chargers_img.jpg",
+    src:"./img/solar_powered_ev_chargers_img.jpg",
     name:"solar-powered ev chargers",
     abtPro:"Charge your electric vehicle with clean, solar energy for a greener commute."
   },
   {
-    src:"/img/wind_power_turbines_img.jpg",
+    src:"./img/wind_power_turbines_img.jpg",
     name:"wind-powered turbine",
     abtPro:"Eco-friendly water pumping systems powered by wind energy, ideal for rural areas."
   },
   {
-    src:"/img/super_capacitors_img.png",
+    src:"./img/super_capacitors_img.png",
     name:"super capacitors",
     abtPro:"High-capacity energy storage solutions for quick charge and discharge applications."
   }
